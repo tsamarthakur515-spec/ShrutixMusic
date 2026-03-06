543c312d974c70af2c6a01327660477b2d210794
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-03-06 19:51:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/5c8d3994b93174a57d7f3-608459ffaf02cf2a9b.jpg"
+    "START_IMG_URL", "https://i.ibb.co/VpwbNQLT/photo-2026-03-06-14-17-00-7614147198061641752.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/c8b09659c837cd9142601-f594bfd29076e79944.jpg"
