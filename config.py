7a22aaa97c7ca34d6ac319177f26d1dc2b1707ab
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-04-10 21:22:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,22 +7,22 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("39005479"))
+API_HASH = getenv("c2e7265b2e96025adcc4731c2c1e5ba5")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("8357466207:AAH8uOVpXPyiHrzoNJTt2hJqES8gWjPGuQA")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+MONGO_DB_URI = getenv("mongodb+srv://nubcoders:nubcoders@music.8rxlsum.mongodb.net/?retryWrites=true&w=majority&appName=music")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID"))
+LOGGER_ID = int(getenv("-1003807388230"))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 7574330905))
+OWNER_ID = int(getenv("OWNER_ID", 7724452546))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/suruchisupport")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+fYnrOJSQP9I4ODlh")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/suruchi_network")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+4eTddUofQDlkYjhl")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://i.ibb.co/VpwbNQLT/photo-2026-03-06-14-17-00-7614147198061641752.jpg"
+    "START_IMG_URL", "https://ibb.co/DH4PBv55"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/nacfzm.jpg"
